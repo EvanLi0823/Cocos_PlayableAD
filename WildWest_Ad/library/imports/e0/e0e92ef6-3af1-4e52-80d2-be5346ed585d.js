50cc955c6cadb5a66b79607499b14ed130bf03ce
Huohua1518 <1923363627@qq.com>
2025-08-04 12:30:15 +0800

    忽略缓存文件，更换 UI

--- WildWest_Ad/library/imports/e0/e0e92ef6-3af1-4e52-80d2-be5346ed585d.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, 'e0e9272OvFOUoDSvlNG7Vhd', 'DataModel');
-// script/Model/DataModel.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
--- WildWest_Ad/library/imports/f8/f8c025dc-99a3-49b0-a565-042fc0653f2b.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, 'f8c02XcmaNJsKVlBC/AZT8r', 'PIDelegate');
-// script/PlatformInteraction/PIDelegate.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
